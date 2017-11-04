487d8144258ad1e4f9185015b67f5a05406cf231
lepidopterane-atsmith <sabowitz@smith.edu>
2017-11-04 05:11:54 -0400

    Fiery V1.0.0
    This implements firebase database for prompt storage and retrieval.
    Coming soon: Random choice that actually works.

---
+++ InspiWriter/app/src/main/java/com/example/inspiwriter/DataWrangler.java
@@ -0,0 +1,25 @@
+package com.example.inspiwriter;
+
+/**
+ * Created by arthurabowitz on 11/4/17.
+ */
+
+public class DataWrangler {
+    public String name;
+    public String text;
+
+    public DataWrangler(){
+        text = "";
+        name = "";
+    }
+
+    public DataWrangler(String n, String t){
+      text = t;
+      name = n;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ InspiWriter/app/src/main/java/com/example/inspiwriter/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.inspiwriter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("messages");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.textView);
+        final ArrayList<DataWrangler> prompts = new ArrayList<DataWrangler>();
+
+        System.out.println(myRef.getKey() + " print pls");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    DataWrangler foo = ds.getValue(DataWrangler.class);
+                    System.out.println(foo);
+                    prompts.add(foo);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println(databaseError);
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("but with a strong female protag");
+                TextView promptView = ()
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
